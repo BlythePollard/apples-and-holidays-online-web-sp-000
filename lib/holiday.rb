698e5f85d38bf26de6ff143e90e13fd5e3ec6783
Blythe Pollard <blytheepollard@gmail.com>
2019-09-23 01:16:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,6 @@ def all_supplies_in_holidays(holiday_hash)
      
      #split_holiday = holiday.to_s.split
      #split_holiday.each do |holidays| 
-     
      #  holidays.each do |caps_holiday|
       #   multi_word = caps_holiday.capitalize!.join
        #  puts "  " + multi_word + ": " + data.join(", ")
